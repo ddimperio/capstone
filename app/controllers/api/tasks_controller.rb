9f49d6bb992fdcd366d082d3bb07602f1b31427b
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-11-26 14:12:29 -0600

    Add Tasks Controller & Routes

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,51 @@
+class Api::TasksController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    # @tasks = Task.all
+    @tasks = current_user.tasks
+
+    render "index.json.jbuilder"
+  end
+
+  def show 
+    input_id = params["id"]
+    @task = Task.find_by(id: params[:id])
+
+    render "show.json.jbuilder"
+  end
+
+  def create
+    @task = Task.new(
+      task: params["input_task"],
+      notes: params["input_notes"],
+      priority: params["input_priority"],
+      status: params["input_status"],
+      user_id: params["input_userid"]
+      )
+    if @task.save
+      render "show.json.jbuilder"
+    else 
+      render json: {errors: @task.errors.full_messages}, status: :inprocessable_entity
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.task = params["input_task"] || @task.task
+    @task.notes = params["input_notes"] || @task.notes
+    @task.priority = params["input_priority"] || @task.priority
+    @task.status = params["input_status"] || @task.status
+    if @task.save
+      render "show.json.jbuilder"
+    else 
+      render json: {errors: @task.errors.full_messages}, status: :inprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
+    render json: {message: "Task has been deleted"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get "/tasks" => "tasks#index"
+    post "/tasks" => "tasks#create"
+    get "tasks/:id" => "tasks#show"
+    patch "tasks/:id" => "tasks#update"
+    delete "tasks/:id" => "tasks#destroy"
+  end
 end
