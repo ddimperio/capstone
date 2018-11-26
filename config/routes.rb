1c93cf547f64d38c4111a1eff19b9b2a4ea234ff
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-11-26 14:23:15 -0600

    Add bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routipng.html
   namespace :api do
     get "/tasks" => "tasks#index"
     post "/tasks" => "tasks#create"
