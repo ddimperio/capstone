bc1a4f331cbf6040d55afb00f66a7351c89c18f2
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-12-19 20:22:56 -0600

    Final

--- app/controllers/api/slacks_controller.rb
+++ app/controllers/api/slacks_controller.rb
@@ -1,7 +1,8 @@
 class Api::SlacksController < ApplicationController
   def create
     task = params[:data]
-    response = HTTP.post("https://hooks.slack.com/services/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", :json => {'text' => task[:task]})
+    final_text = "#{task[:task]} (Notes: #{task[:notes]})"
+    response = HTTP.post("https://hooks.slack.com/services/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", :json => {'text' => final_text})
     render json: task
   end
 end
